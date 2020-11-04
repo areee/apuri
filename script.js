f018808f813f3e57b4c672e8b0028a45b67edab1
areee <10089872+areee@users.noreply.github.com>
2020-11-04 20:44:27 +0200

    script: add the 1st working version

---
+++ script.js
@@ -0,0 +1,27 @@
+let prefixInput = document.getElementById("prefixInput");
+let suffixInput = document.getElementById("suffixInput");
+
+let editableTextArea = document.getElementById("editableTextArea");
+let resultTextArea = document.getElementById("resultTextArea");
+
+let goButton = document.getElementById("goButton");
+goButton.addEventListener("click", generateResultText);
+
+function generateResultText() {
+  let lines = editableTextArea.value.split("\n");
+
+  let result = "";
+
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i];
+    let lineResult = prefixInput.value + line + suffixInput.value;
+    console.log(lineResult);
+    result += lineResult;
+
+    if (i != lines.length - 1) {
+      result += "\n";
+    }
+  }
+  console.log("result: \n" + result);
+  resultTextArea.value = result;
+}
